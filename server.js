80e14583b1c1359f9275f8bc7a1380d857067f0c
windo <herwindo.artono@gmail.com>
2020-07-04 11:32:40 +0700

    feat: added .env

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser =require('body-parser');
 const axios = require('axios');
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 3300;
-const key = ''
+const key = process.env.MAPS_API;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
