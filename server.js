be47eb44fe8db44af82d6a0a628ca4bb01f8f873
windo <herwindo.artono@gmail.com>
2020-07-04 11:51:04 +0700

    feat: API for textsearch

--- server.js
+++ server.js
@@ -6,15 +6,50 @@ const axios = require('axios');
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3300;
-const key = process.env.MAPS_API;
+const mapsKey = process.env.MAPS_API;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/places/:input', (req, res) => {
+app.get('/place/:input', (req, res) => {
     const input = req.params.input;
     let result = null;
-    axios.get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${input}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${key}`)
+    axios.get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${input}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${mapsKey}`)
+        .then(response => {
+           console.log('success', response.data);
+           result = response.data;
+           res.status(200).json(result);
+        })
+        .catch(err => {
+            throw new Error(err);
+        });
+});
+
+app.get('/nearby/:input', (req, res) => {
+    const input = req.params.input;
+    let result = null;
+    axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&rankby=distance&type=food&key=${mapsKey}`)
+    .then(response => {
+        console.log('success', response.data);
+        result = response.data;
+        res.status(200).json(result);
+     })
+     .catch(err => {
+         throw new Error(err);
+     });
+});
+
+app.get('/search/:input', (req, res) => {
+    const _input = req.params.input;
+    let result = null;
+    axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json`, {
+        params: {
+            input: _input,
+            inputtype: 'textquery',
+            type: 'food',
+            key: mapsKey
+        }
+    })
         .then(response => {
            console.log('success', response.data);
            result = response.data;
